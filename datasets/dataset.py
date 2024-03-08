2fa8ae81f95c8c1d5c1954f8777b08427adfe9be
guanyuezhen <31332365+guanyuezhen@users.noreply.github.com>
2024-03-08 17:28:09 +0800

    Update dataset.py
    add def crop_image_into_patches(image, patch_size):

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -1,5 +1,5 @@
 import cv2
-import numpy
+import numpy as np
 import torch.utils.data
 
 
@@ -16,36 +16,36 @@ class Dataset(torch.utils.data.Dataset):
         self.file_list = open(file_root + '/' + dataset + '/list/' + dataset + '.txt').read().splitlines()
         self.pre_images = [file_root + '/' + dataset + '/A/' + x for x in self.file_list]
         self.post_images = [file_root + '/' + dataset + '/B/' + x for x in self.file_list]
-        if label_patch_size == 32:
-            self.patch_gts = [file_root + '/' + dataset + '/label_32/' + x for x in self.file_list]
-            self.gts = [file_root + '/' + dataset + '/label/' + x for x in self.file_list]
-        elif label_patch_size == 64:
-            self.patch_gts = [file_root + '/' + dataset + '/label_64/' + x for x in self.file_list]
-            self.gts = [file_root + '/' + dataset + '/label/' + x for x in self.file_list]
-        elif label_patch_size == 128:
-            self.patch_gts = [file_root + '/' + dataset + '/label_128/' + x for x in self.file_list]
-            self.gts = [file_root + '/' + dataset + '/label/' + x for x in self.file_list]
-        elif label_patch_size == 256:
-            self.patch_gts = [file_root + '/' + dataset + '/label_256/' + x for x in self.file_list]
-            self.gts = [file_root + '/' + dataset + '/label/' + x for x in self.file_list]
-        else:
-            self.patch_gts = [file_root + '/' + dataset + '/label/' + x for x in self.file_list]
-            self.gts = [file_root + '/' + dataset + '/label/' + x for x in self.file_list]
+        self.label_patch_size = label_patch_size
+        self.gts = [file_root + '/' + dataset + '/label/' + x for x in self.file_list]
         self.transform = transform
 
     def __len__(self):
         return len(self.pre_images)
+    
+    @staticmethod
+    def crop_image_into_patches(image, patch_size):
+        image = np.ceil(image / 255)
+        width, height = image.shape # height = width
+        # Reshape the original matrix into submatrices
+        image_patches = image.reshape(width//patch_size, patch_size, -1, patch_size).swapaxes(1,2)
+        # Check if there is any 1 in each submatrix
+        change_exists = np.any(image_patches, axis=(2,3))
+        # Set elements to 1 in submatrices where 1 exists
+        new_image = np.repeat(np.repeat(change_exists, patch_size, axis=0), patch_size, axis=1).astype(int)
+        new_image = new_image * 255
+
+        return new_image
 
     def __getitem__(self, idx):
         pre_image_name = self.pre_images[idx]
         post_image_name = self.post_images[idx]
-        patch_label_name = self.patch_gts[idx]
         label_name = self.gts[idx]
         pre_image = cv2.imread(pre_image_name)
         post_image = cv2.imread(post_image_name)
-        patch_label = cv2.imread(patch_label_name, 0)
         label = cv2.imread(label_name, 0)
-        img = numpy.concatenate((pre_image, post_image), axis=2)
+        patch_label = self.crop_image_into_patches(label, self.label_patch_size)
+        img = np.concatenate((pre_image, post_image), axis=2)
         if self.transform:
             [img, patch_label, label] = self.transform(img, patch_label, label)
 
